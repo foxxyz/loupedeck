d3061d31202131b89a41f500084ef3a9637afc2f
foxxyz <foxxyz@gmail.com>
2022-01-29 23:04:34 -0800

    feat: add magic byte parser for handling serial input

---
+++ parser.js
@@ -0,0 +1,37 @@
+const { Transform } = require('stream')
+
+// Parser to split incoming serial data by a magic byte sequence
+// followed by a length
+class MagicByteLengthParser extends Transform {
+    constructor({ magicByte, ...args }) {
+        super(args)
+        this.delimiter = magicByte
+        this.buffer = Buffer.alloc(0)
+        this.nextLength = null
+    }
+
+    _transform(chunk, encoding, cb) {
+        let data = Buffer.concat([this.buffer, chunk])
+        let position
+        while ((position = data.indexOf(this.delimiter)) !== -1) {
+            // We need to at least be able to read the length byte
+            if (data.length < position + 2) break
+            const nextLength = data[position + 1]
+            // Make sure we have enough bytes to meet this length
+            const expectedEnd = position + nextLength + 2
+            if (data.length < expectedEnd) break
+            this.push(data.slice(position, expectedEnd))
+            data = data.slice(expectedEnd)
+        }
+        this.buffer = data
+        cb()
+    }
+
+    _flush(cb) {
+        this.push(this.buffer)
+        this.buffer = Buffer.alloc(0)
+        cb()
+    }
+}
+
+module.exports = MagicByteLengthParser
